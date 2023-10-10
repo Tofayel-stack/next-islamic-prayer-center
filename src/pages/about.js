be89da39fa5652ede53df16f21256bc12a93ad13
Tofayel Hossain <78659570+Tofayel-stack@users.noreply.github.com>
2023-10-10 23:11:24 +0600

    dsign done

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,63 @@ import RootLayout from "@/components/layout/RootLayout";
 const About = () => {
     return (
         <div>
-            this is about
+            {/* page banner section  */}
+            <div className={`bg-[url("https://i.ibb.co/ZmThCF5/services-head-1.png")] h-[469px] }`}>
+                <div className='text-white  flex flex-col justify-center items-center  h-96'>
+                    <h1 className='text-5xl font-semibold'>About EKRAH</h1>
+                    <p className='py-6'>home? about </p>
+                </div>
+            </div>
+
+
+               {/* all service show here .. data will come from database */}
+               <div className='lg:w-[70rem] m-auto'>
+                  {/* title */}
+                    <div >
+                    <h1 className='text-center my-6 font-sans text-5xl font-bold  md:mx-auto'> Board Of Scholors</h1>
+                        <p className='text-amber-700 my-4 text-2xl text-center'>Our scholar whose knowledge is useful for others</p>
+                    </div>
+
+                <div className='mt-16 grid grid-cols-1 gap-2 my-10 sm:grid-cols-3 lg:grid-cols-3'>
+
+
+                   {/* single card  */}
+                    <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+                        <img src="https://source.unsplash.com/random/300x300/?2" alt="pic" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                        <div className="flex flex-col justify-between p-6 space-y-8">
+                            <div className="space-y-2 text-center">
+                                <h2 className="text-2xl font-semibold">Bin Baj Rahmahullah</h2>
+                                <p className="dark:text-gray-100">Hafiz Quarn Scolar</p>
+                            </div>
+                        </div>
+                    </div>
+                   {/* single card  */}
+                    <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+                        <img src="https://source.unsplash.com/random/300x300/?2" alt="pic" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                        <div className="flex flex-col justify-between p-6 space-y-8">
+                            <div className="space-y-2 text-center">
+                                <h2 className="text-2xl font-semibold">Bin Baj Rahmahullah</h2>
+                                <p className="dark:text-gray-100">Hafiz Quarn Scolar</p>
+                            </div>
+                        </div>
+                    </div>
+                   {/* single card  */}
+                    <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+                        <img src="https://source.unsplash.com/random/300x300/?2" alt="pic" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                        <div className="flex flex-col justify-between p-6 space-y-8">
+                            <div className="space-y-2 text-center">
+                                <h2 className="text-2xl font-semibold">Bin Baj Rahmahullah</h2>
+                                <p className="dark:text-gray-100">Hafiz Quarn Scolar</p>
+                            </div>
+                        </div>
+                    </div>
+               
+
+
+                </div> 
+            </div>
+
+
         </div>
     );
 };
--- src/pages/events.js
+++ src/pages/events.js
@@ -4,7 +4,29 @@ import React from 'react';
 const Events = () => {
     return (
         <div>
-            Events
+               {/* page banner section  */}
+               <div className={`bg-[url("https://i.ibb.co/ZmThCF5/services-head-1.png")] h-[469px] }`}>
+                <div className='text-white  flex flex-col justify-center items-center  h-96'>
+                    <h1 className='text-5xl font-semibold'>Our Events</h1>
+                    <p className='py-6'>home? events </p>
+                </div>
+            </div>
+
+            {/*photo gallery  */}
+            <section className="py-6 dark:bg-gray-800 dark:text-gray-50">
+                <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
+                    <img src="https://source.unsplash.com/random/301x301/" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?0" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?1" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?2" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?3" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?4" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?5" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?6" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?7" />
+                    <img src="https://source.unsplash.com/random/302x302/" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square" />
+                </div>
+            </section>
         </div>
     );
 };
